a7c9fad7d6f968b3b085e7513501be2c434ea147
gracie12305 <kwofib5@gmail.com>
2025-07-08 20:49:49 +0200

    week1

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -13,7 +13,18 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        //create an arrayof type double with a given legnth.
+        double[] result = new double[length];
+
+        //a for loop to populate each index of the array.
+        for (int i = 0; i < length; i++)
+        {
+            //at each index i, store the value number *(i+1)
+            result[i] = number * (i + 1);
+        }
+
+
+            return result;
     }
 
     /// <summary>
@@ -29,5 +40,21 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        //calculate the index to split the list
+        int splitIndex = data.Count - amount;
+
+        //use GetRange to get the first elements.
+        List<int> firstPart = data.GetRange(0, splitIndex);
+
+        //use GetRange to get the last alements which will go to the front
+        List<int> lastPart = data.GetRange(splitIndex, amount);
+
+        //clear the original list
+        data.Clear();
+
+        //adding the rotated part to the list in the right order
+        data.AddRange(lastPart);
+        data.AddRange(firstPart);
     }
 }
